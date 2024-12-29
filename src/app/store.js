90674e4832ad2fff07d917ac44f2f7ab58a4ded0
aditya2145 <adityasharma05986.com>
2024-12-29 22:05:29 +0530

    push code

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import resumeReducer from '../features/resume/resumeSlice';
-
-export default configureStore({
-    reducer: {
-        resume: resumeReducer,
-    },
-});
